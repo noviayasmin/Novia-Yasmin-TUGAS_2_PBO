b90c4b4669b0c04b8d53100dd8fb18acacf2ed4e
noviayasmin <noviayasmin14@gmail.com>
2015-10-10 12:50:52 +0700

    TUGAS2_PBO

---
+++ src/OperatorAritmatika.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class OperatorAritmatika {
+
+    static int penjumlahan(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public int hasil;
+    double pembagia;
+    int penjumlahan(int a, int b) {
+        hasil= a + b;
+        return hasil;
+    }		
+    
+    int pengurangan(int a, int b) {
+        hasil=a - b;
+        return hasil;
+    }
+    
+    int perkalian(int a, int b){
+        hasil=a * b;
+        return hasil;
+    }
+    
+    double pembagian(double c, double d){
+       double jumlah;
+      return  jumlah = c / d;
+        
+      
+        
+    }
+
+   
+}
